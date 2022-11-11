a4794d865da661d58bc569d40a9509baf4e81520
Allen Joseph <unknownhackerworld@gmail.com>
2022-11-11 23:02:32 +0530

    modified:   SchoolProject.py

--- SchoolProject.py
+++ SchoolProject.py
@@ -24,20 +24,25 @@ while True:
                 else:
                     a = str(input(f'Enter Your {info}: '))
                 single[info] = a
-                infos.update({ single['ID'] : infos })
-            print(str(infos),"\n\n",str(single))
+                infos.update({ single['ID'] : single })
+            print(str(infos.items()))
             print("\n"*5)
             
         elif (ch==2):
-            id = str(input("Please Enter Your Id: "))
+            id = str(input("Please Enter Your ID: "))
             if id in infos:
                 prob = str(input("Enter A Brief Explanation Of Your Problem: "))
-                time_slot = input("Enter Time Slot (Today) HH:MM")
-                info[id] = {"problem" : prob, "time":time_slot}
-                print(info.getkeys())
+                time_slot = input("Enter Time Slot (Today) HH:MM: ")
+                infos[id]["problems"] = prob
+                infos[id]["time"] = time_slot
+                
 
             else:
                 print("Please Create Admission")
             print("\n"*5)
 
-        #elif(ch==3):
+        elif(ch==3):
+            id = str(input("Please Enter Your ID: "))
+            if id in infos:
+                print("Problems\t\tTime Slot")
+                print(infos[id]["problems"],"\t\t",infos[id]["time"])
